94627a8734a389f4b32eb9a820b6a29594da307d
gamalielhere <gamalielgad1234@gmail.com>
2018-04-26 10:45:18 -0700

    Add test addsettoken method

--- app/scripts/tokenContract.js
+++ app/scripts/tokenContract.js
@@ -10,6 +10,10 @@ tokenContract.prototype.getDataString = function(func, inputs) {
   var typeName = ethUtil.solidityUtils.extractTypeName(fullFuncName);
   var types = typeName.split(',');
   types = types[0] === "" ? [] : types;
+  console.log(fullFuncName)
+  console.log(funcSig)
+  console.log(typeName)
+  console.log(types)
   return '0x' + funcSig + ethUtil.solidityCoder.encodeParams(types, inputs);
 };
 
@@ -38,6 +42,32 @@ tokenContract.prototype.getAllBalance = function(to, params) {
     };
   })
 };
+
+tokenContract.prototype.addSetToken = function(to, from, params) {
+  const name = params.name ? params.name : '';
+  const symbol = params.symbol ? params.symbol : '';
+  const address = params.address ? params.address : '';
+  const decimal = params.decimal ? params.decimal : 0;
+  const website = params.website ? params.website : '';
+  const email = params.email ? params.email : '';
+  const nonce = params.nonce ? params.nonce : 0;
+
+  // ajaxReq.getEthCall({
+  // 	to: to,
+	// 	value: 0x0,
+	// 	gas: 21000,
+	// 	nonce: nonce,
+	// 	gasPrice: 1000000000, // 1 gwei
+  //   data: this.getDataString(this.tokensAbi.getAllBalance, [address, name, website, email, count])
+  // }, function(data) {
+  //   if(data.error || data.data === '0x') console.log(data, "error");
+  //   else {
+  //     return ethFuncs.decode(data.data);
+  //   };
+  // })
+  //
+  console.log(this.getDataString(this.tokensAbi.addSetToken, [name, symbol, address, decimal, website, email, nonce]));
+}
 
 tokenContract.prototype.getToken = function(to, address) {
   ajaxReq.getEthCall({
